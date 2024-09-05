a53865274a9874bc333aa5142b32400ab7533c83
Daniel Fabiani <danielfabiani.dev@gmail.com>
2024-09-05 17:18:22 -0300

    primeras configuraciones del template base

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
 import { cn } from "../lib/utils";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const fontSans = FontSans({ subsets: ["latin"], variable: "--font-sans" });
 
@@ -18,15 +19,22 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className="scroll-smooth [&_*]:transition-colors dark"
+      className="scroll-smooth [&_*]:transition-colors"
     >
       <body
         className={cn(
-          "min-h-screen bg-background font-sans antialiased",
+          "min-h-screen bg-background font-sans antialiased ",
           fontSans.variable
         )}
       >
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { ThemeToggle } from "@/components/theme-toggle";
+
 export default function Home() {
   return (
     <section className="py-24">
       <div className="container mx-auto">
+        <ThemeToggle />
         <h1 className="text-3xl font-bold">Next TS Starter</h1>
       </div>
     </section>
